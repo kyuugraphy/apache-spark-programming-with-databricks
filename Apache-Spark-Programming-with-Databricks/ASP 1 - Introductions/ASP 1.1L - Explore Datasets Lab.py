3e0c3ec6375ddf9d3d4f7bfdf3e38462b454b88b
kyuugraphy <kyuugraphy>
2024-02-09 14:47:01 +0000

    testing commit 1st exercise

--- Apache-Spark-Programming-with-Databricks/ASP 1 - Introductions/ASP 1.1L - Explore Datasets Lab.py
+++ Apache-Spark-Programming-with-Databricks/ASP 1 - Introductions/ASP 1.1L - Explore Datasets Lab.py
@@ -1,6 +1,6 @@
 # Databricks notebook source
 # MAGIC %md-sandbox
-# MAGIC 
+# MAGIC
 # MAGIC <div style="text-align: center; line-height: 0; padding-top: 9px;">
 # MAGIC   <img src="https://databricks.com/wp-content/uploads/2018/03/db-academy-rgb-1200px.png" alt="Databricks Learning" style="width: 600px">
 # MAGIC </div>
@@ -9,13 +9,13 @@
 
 # MAGIC %md
 # MAGIC # Explore Datasets Lab
-# MAGIC 
+# MAGIC
 # MAGIC We will use tools introduced in this lesson to explore the datasets used in this course.
-# MAGIC 
+# MAGIC
 # MAGIC ### BedBricks Case Study
 # MAGIC This course uses a case study that explores clickstream data for the online mattress retailer, BedBricks.  
 # MAGIC You are an analyst at BedBricks working with the following datasets: **`events`**, **`sales`**, **`users`**, and **`products`**.
-# MAGIC 
+# MAGIC
 # MAGIC ##### Tasks
 # MAGIC 1. View data files in DBFS using magic commands
 # MAGIC 1. View data files in DBFS using dbutils
@@ -30,26 +30,26 @@
 
 # MAGIC %md ### 1. List data files in DBFS using magic commands
 # MAGIC Use a magic command to display files located in the DBFS directory: **`dbfs:/databricks-datasets`**
-# MAGIC 
+# MAGIC
 # MAGIC <img src="https://files.training.databricks.com/images/icon_hint_32.png" alt="Hint"> You should see several datasets that come pre-installed in Databricks such as: **`COVID`**, **`adult`**, and **`airlines`**.
 
 # COMMAND ----------
 
 # TODO
-<FILL_IN>
+dbutils.fs.ls("/databricks-datasets")
 
 # COMMAND ----------
 
 # MAGIC %md ### 2. List data files in DBFS using dbutils
 # MAGIC - Use **`dbutils`** to get the files at the directory above and save it to the variable **`files`**
 # MAGIC - Use the Databricks display() function to display the contents in **`files`**
-# MAGIC 
+# MAGIC
 # MAGIC <img src="https://files.training.databricks.com/images/icon_hint_32.png" alt="Hint"> You should see several datasets that come pre-installed in Databricks such as: **`COVID`**, **`adult`**, and **`airlines`**.
 
 # COMMAND ----------
 
 # TODO
-files = dbutils.FILL_IN
+files = dbutils.fs.ls("/databricks-datasets")
 display(files)
 
 # COMMAND ----------
@@ -59,7 +59,7 @@ display(files)
 # MAGIC - Create the **`sales`** table using the spark-context variable **`c.sales_path`**
 # MAGIC - Create the **`products`** table using the spark-context variable **`c.products_path`**
 # MAGIC - Create the **`events`** table using the spark-context variable **`c.events_path`**
-# MAGIC 
+# MAGIC
 # MAGIC <img src="https://files.training.databricks.com/images/icon_hint_32.png"> Hint: We created the **`events`** table in the previous notebook but in a different database.
 
 # COMMAND ----------
@@ -78,24 +78,24 @@ display(files)
 # MAGIC - What products are available for purchase at BedBricks?
 # MAGIC - What is the average purchase revenue for a transaction at BedBricks?
 # MAGIC - What types of events are recorded on the BedBricks website?
-# MAGIC 
+# MAGIC
 # MAGIC The schema of the relevant dataset is provided for each question in the cells below.
 
 # COMMAND ----------
 
 # MAGIC %md 
 # MAGIC #### 4.1: What products are available for purchase at BedBricks?
-# MAGIC 
+# MAGIC
 # MAGIC The **`products`** dataset contains the ID, name, and price of products on the BedBricks retail site.
-# MAGIC 
+# MAGIC
 # MAGIC | field | type | description
 # MAGIC | --- | --- | --- |
 # MAGIC | item_id | string | unique item identifier |
 # MAGIC | name | string | item name in plain text |
 # MAGIC | price | double | price of item |
-# MAGIC 
+# MAGIC
 # MAGIC Execute a SQL query that selects all from the **`products`** table. 
-# MAGIC 
+# MAGIC
 # MAGIC <img src="https://files.training.databricks.com/images/icon_hint_32.png" alt="Hint"> You should see 12 products.
 
 # COMMAND ----------
@@ -106,10 +106,10 @@ display(files)
 # COMMAND ----------
 
 # MAGIC %md #### 4.2: What is the average purchase revenue for a transaction at BedBricks?
-# MAGIC 
+# MAGIC
 # MAGIC The **`sales`** dataset contains order information representing successfully processed sales.  
 # MAGIC Most fields correspond directly with fields from the clickstream data associated with a sale finalization event.
-# MAGIC 
+# MAGIC
 # MAGIC | field | type | description|
 # MAGIC | --- | --- | --- |
 # MAGIC | order_id | long | unique identifier |
@@ -119,9 +119,9 @@ display(files)
 # MAGIC | purchase_revenue_in_usd | double | total revenue from order |
 # MAGIC | unique_items | long | number of unique products in the order |
 # MAGIC | items | array | provided as a list of JSON data, which is interpreted by Spark as an array of structs |
-# MAGIC 
+# MAGIC
 # MAGIC Execute a SQL query that computes the average **`purchase_revenue_in_usd`** from the **`sales`** table.
-# MAGIC 
+# MAGIC
 # MAGIC <img src="https://files.training.databricks.com/images/icon_hint_32.png" alt="Hint"> The result should be **`1042.79`**.
 
 # COMMAND ----------
@@ -132,10 +132,10 @@ display(files)
 # COMMAND ----------
 
 # MAGIC %md #### 4.3: What types of events are recorded on the BedBricks website?
-# MAGIC 
+# MAGIC
 # MAGIC The **`events`** dataset contains two weeks worth of parsed JSON records, created by consuming updates to an operational database.  
 # MAGIC Records are received whenever: (1) a new user visits the site, (2) a user provides their email for the first time.
-# MAGIC 
+# MAGIC
 # MAGIC | field | type | description|
 # MAGIC | --- | --- | --- |
 # MAGIC | device | string | operating system of the user device |
@@ -148,9 +148,9 @@ display(files)
 # MAGIC | event_name | string | name of events as registered in clickstream tracker |
 # MAGIC | items (item_id, item_name, price_in_usd, quantity, item_revenue in usd, coupon)| array | an array of structs for each unique item in the user’s cart |
 # MAGIC | ecommerce (total_item_quantity, unique_items, purchase_revenue_in_usd)  |  struct  | purchase data (this field is only non-null in those events that correspond to a sales finalization) |
-# MAGIC 
+# MAGIC
 # MAGIC Execute a SQL query that selects distinct values in **`event_name`** from the **`events`** table
-# MAGIC 
+# MAGIC
 # MAGIC <img src="https://files.training.databricks.com/images/icon_hint_32.png" alt="Hint"> You should see 23 distinct **`event_name`** values.
 
 # COMMAND ----------
